8d0a4082ff89514b71dc09f6581dd5cdb8545590
oliver <labelwendell@gmail.com>
2023-10-10 20:51:13 -0300

    alteração do dia 10/10 , fiz models

--- app_biblioteca/models.py
+++ app_biblioteca/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Livro(models.Model):
+    Nome_do_Livro = models.CharField(max_length=200)
+    Departamento = models.CharField(max_length=200)
+    Autor = models.CharField(max_length=200)
+    Quantidade_páginas =models.PositiveSmallIntegerField()
+    idioma =models.CharField(max_length=200)
+
+
+    def __str__(self):
+      return self.Nome_do_Livro
